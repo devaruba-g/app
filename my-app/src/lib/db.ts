1216bdb15f0a1c9f7949d319f73214146558d1b6
Devaruba G <devarubag@gmail.com>
2025-10-17 12:53:38 +0530

    Add files

--- my-app/src/lib/db.ts
+++ my-app/src/lib/db.ts
@@ -5,21 +5,10 @@ import { Lucia } from "lucia";
 import { Mysql2Adapter } from "@lucia-auth/adapter-mysql";
 import { createPool } from "mysql2/promise";
 
-const DATABASE_URL = "mysql://root:WriKIrvuNTqyWuNeoIwAIYDgZdMvZlif@interchange.proxy.rlwy.net:36698/railway";
-
+const DATABASE_URL="mysql://root:emJvDumVmnUNebxHZaUyYFQeELnhpFCo@shuttle.proxy.rlwy.net:48612/railway"
 export const db = createPool({
-  host: 'interchange.proxy.rlwy.net',
-  port: 36698,
-  user: 'root',
-  password: 'WriKIrvuNTqyWuNeoIwAIYDgZdMvZlif',
-  database: 'railway',
-  ssl: { rejectUnauthorized: false },
-  connectionLimit: 5,
-  connectTimeout: 30000,
-  waitForConnections: true,
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 10000
+  uri: DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 export const auth = new Lucia(
